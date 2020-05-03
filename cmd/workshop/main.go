0305df18f68b0688ffb0010b1c7083c758bff710
Denis Barabanshchikov <denisb1989@web.de>
2020-05-03 21:31:46 +0200

    add config

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -3,22 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"workshop/internal/config"
 
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 
 	"workshop/internal/handler"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	h := handler.NewHandler()
-
 	r := chi.NewRouter()
-
 	r.Get("/hello", h.Hello)
 
 	log.Print("starting server")
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal(err)
 	}
