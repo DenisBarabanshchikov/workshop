abb4dc370cbfddc9a2dd139b4735de0303d9b218
Denis Barabanshchikov <denisb1989@web.de>
2020-05-06 22:16:47 +0200

    add joke api

---
+++ internal/api/jokes/jokes.go
@@ -0,0 +1,34 @@
+package jokes
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"workshop/internal/api"
+)
+
+const apiPath = "/api?format=json"
+
+type JokeClient struct {
+	url string
+}
+
+func (jc *JokeClient) GetJoke() (*api.JokeResponse, error) {
+	urlPath := jc.url + apiPath
+
+	res, err := http.Get(urlPath)
+	if err != nil {
+		return nil, err
+	} else if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("api request error %v", res.StatusCode)
+	}
+
+	var data api.JokeResponse
+
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
